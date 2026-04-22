0ebae282eb0bc1cfa0989eb448d2f362d1251fac
Danil Lihanov <danillihanov321@gmail.com>
2026-04-22 22:26:37 +0300

    Add tree.h and bitbuffer.h header files

--- include/tree.h
+++ include/tree.h
@@ -1,1 +1,15 @@
-//add
+#pragma once
+
+struct Node {
+    unsigned char symbl;
+    int count;
+    Node *left, *right;
+
+    Node(unsigned char symbl, int count) : symbl(symbl), count(count), left(nullptr), right(nullptr) {}
+
+    bool isLeaf() {
+        return (left == nullptr && right == nullptr);
+    }
+};
+
+void deleteTree(Node *root);
