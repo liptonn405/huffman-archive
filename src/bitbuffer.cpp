f2484fafb2eebf27f1e02534feefd8c550b4ff64
Danil Lihanov <danillihanov321@gmail.com>
2026-04-27 08:48:33 +0300

    Add bitbuffer.cpp and test_bitbuffer.cpp

--- src/bitbuffer.cpp
+++ src/bitbuffer.cpp
@@ -1,1 +1,35 @@
-//add
+#include "bitbuffer.h"
+
+void BitWriter::writeBit(uint8_t bit, std::ofstream &out) {
+    buffer = buffer | (bit << (7 - bitCount));
+    bitCount++;
+    if (bitCount == 8) {
+        out.write(reinterpret_cast<const char*>(&buffer), 1);
+        buffer = 0;
+        bitCount = 0;
+    }
+}
+
+void BitWriter::flush(std::ofstream &out) {
+    if (bitCount > 0) {
+        out.write(reinterpret_cast<const char*>(&buffer), 1);
+        buffer = 0;
+        bitCount = 0;
+    }
+}
+
+int BitReader::readBit(std::ifstream &in) {
+    if (bitCount == 0) {
+        char byte = 0;
+        in.read(&byte, 1);
+        if (in.gcount() == 0) {
+            return -1;
+        }
+        buffer = byte;
+        bitCount = 8;
+    }
+    int bit = (buffer >> 7) & 1;
+    buffer = buffer << 1;
+    bitCount--;
+    return bit;
+}
--- tests/test_main.cpp
+++ tests/test_main.cpp
@@ -1,13 +1,23 @@
 #include "test_framework.h"
 
+//huffman.cpp
 void test_countFrequency();
 void test_buildTree();
 void test_buildCodeTable();
+
+//bitbuffer.cpp
+void test_bitbuffer();
+void test_bitReader();
+void test_flush();
 
 int main() {
     RUN_TEST(test_countFrequency);
     RUN_TEST(test_buildTree);
     RUN_TEST(test_buildCodeTable);
+
+    RUN_TEST(test_bitbuffer);
+    RUN_TEST(test_bitReader);
+    RUN_TEST(test_flush);
 
     std::cout << "\n[PASSED]:" << g_testsPassed << "\n";
     std::cout << "[FAILED]:" << g_testsFailed << "\n";
